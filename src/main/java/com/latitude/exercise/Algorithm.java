8918ac3a6214a2eec2723b52b711e12a926d40c5
Lawrence Colman <LColman@GitHub>
2020-01-27 16:26:11 +1100

    Implemented Java Iterator syntax

--- src/main/java/com/latitude/exercise/Algorithm.java
+++ src/main/java/com/latitude/exercise/Algorithm.java
@@ -1,8 +1,10 @@
 package com.latitude.exercise;
 
+import java.util.Iterator;
+
 public class Algorithm {
 
-    public int calculateMaxProfit(final int[] prices) {
+    public int calculateMaxProfit(final Iterable<Integer> prices) {
         int globalMaxima = 0;  //result
         int minPrice = Integer.MAX_VALUE;
 
@@ -14,6 +16,28 @@ public class Algorithm {
             globalMaxima = Math.max(globalMaxima, localMaxima);
         }
         return globalMaxima;
+    }
+
+    public int calculateMaxProfit(final int[] prices) {
+        return calculateMaxProfit(new Iterable<Integer>() {
+            public Iterator<Integer> iterator() {
+                return new Iterator<Integer>() {
+                    public void remove() {
+                        throw new UnsupportedOperationException("");
+                    }
+
+                    private int index = 0;
+
+                    public boolean hasNext() {
+                        return index < prices.length;
+                    }
+
+                    public Integer next() {
+                        return prices[index++];
+                    }
+                };
+            }
+        });
     }
 
 }
