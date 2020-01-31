5c4eed8053016ac9e36077d49ab83b0fcc66070c
Lawrence Colman <LColman@GitHub>
2020-01-31 15:31:14 +1100

    Tidying up the algorithm code comments for new solution

--- src/main/java/com/latitude/exercise/Algorithm.java
+++ src/main/java/com/latitude/exercise/Algorithm.java
@@ -5,30 +5,30 @@ public class Algorithm {
     public int calculateMaxProfit(final int[] prices) {
         boolean canSell = false;  // flag to indicate ability to sell
         int globalMaxima = 0;  // algorithm result
-        int minPrice = Integer.MAX_VALUE;  // the smallest price
+        int minPrice = Integer.MAX_VALUE;  // the smallest price seen so far
 
         // for each price point in the array
         for (int currentPrice : prices) {
-            // if this price is lower than the lowest price seen so far
+            // if the currentPrice is lower than the lowest price seen so far
             if (currentPrice < minPrice) {
-                // then we update the smallest price to be the current minima
+                // then we update the smallest price to be the currentPrice
                 minPrice = currentPrice;
                 // such an update means we now operate as if this is the time
                 // of the stock purchase, and therefore cannot sell right now
                 canSell = false;
-            } else {
+            } else {  // when the currentPrice is NOT a candidate for purchase
                 // if we are allowed to sell this step, calculate the potential profit
                 if (canSell) {
                     /*
-                    // the maximum obtainable profit from a sale at this current time
-                    // index would be the different between the current price and the
-                    // smallest price seen so far.  This will be zero when the above if
-                    // branch has been executed (i.e. currentPrice - minPrice == 0).
+                     Maximum obtainable profit from a sale at this current time
+                     index would be the difference between the current price and
+                     the smallest price seen so far.  Uses abs for negatives.
                     */
                     int localMaxima = Math.abs(currentPrice - minPrice);
                     if (localMaxima > globalMaxima) {
-                        // if the current maxima at this time step is greater than existing
-                        // globalMaximum then update the profit result as if we sold here
+                        // if the current maxima at this time step is greater
+                        // than existing globalMaximum profit result then
+                        // update the profit result as if we sold stock here
                         globalMaxima = localMaxima;
                     }
                 }
